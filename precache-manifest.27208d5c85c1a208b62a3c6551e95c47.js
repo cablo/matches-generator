c2e64d874f748b9e49a9e8c4a3500067afd30c49
Karel Cabel <kcabel@vendavo.com>
2020-03-29 12:12:54 +0200

    Updates

---
+++ precache-manifest.27208d5c85c1a208b62a3c6551e95c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919cfcdeb763e6bafd8efbcead94ab44",
+    "url": "/matches-generator/index.html"
+  },
+  {
+    "revision": "1032f9c3ced5434987d0",
+    "url": "/matches-generator/static/css/main.ea663904.chunk.css"
+  },
+  {
+    "revision": "6e53673f30bac326dcd0",
+    "url": "/matches-generator/static/js/2.abff978e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/matches-generator/static/js/2.abff978e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1032f9c3ced5434987d0",
+    "url": "/matches-generator/static/js/main.1f4cfc9f.chunk.js"
+  },
+  {
+    "revision": "b7cb86b38e76c1e760e7",
+    "url": "/matches-generator/static/js/runtime-main.cfbadc01.js"
+  }
+]);
